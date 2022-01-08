29cf5b6e917fb67d9b8a6c55429b09a0232d2530
Hemit Rana <77327928+Hemit1409@users.noreply.github.com>
2022-01-08 22:07:47 +0530

    hindi word tokenization

--- tokenizer.py
+++ tokenizer.py
@@ -2,11 +2,13 @@ import os
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-file = open(r"gujaratiDataSet.txt","r+",encoding="utf-8")
-str=file.read()
+with open(r"data/gujaratiDataSet.txt","r+",encoding="utf-8") as file:
+  str = file.read()
 
-file1 = open(r"hindiDataSet.txt","r+",encoding="utf-8")
-str2=file1.read()
+file.close()
+
+with open(r"data/hindiDataSet.txt","r+",encoding="utf-16") as file1:
+  str2 = file1.read()
 
 print (sent_tokenize(str))
 print (sent_tokenize(str2))
@@ -18,17 +20,16 @@ print("printing")
 print(wordlist)
 print(wordlist2)
 
-#file1 = open(r"C:\Users\Hemit\Desktop\token\output.txt","w",encoding="utf-8")
 
-with open(r"gujaratitokens.txt","w",encoding="utf-8") as file2:
+with open(r"data/gujaratitokens.txt","w",encoding="utf-8") as file2:
     for words in wordlist:
         file2.write("%s\n" % words)
 
-with open(r"hinditokens.txt","w",encoding="utf-8") as file3:
+with open(r"data/hinditokens.txt","w",encoding="utf-8") as file3:
     for words2 in wordlist2:
         file3.write("%s\n" % words2)
             
 file1.close
 file2.close
 file3.close
-file.close
+
